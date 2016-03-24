14a34b991448e4b77db0fa2951708c119dbbb2e1
Renato Teixeira <rteixeir@progress.com>
2016-03-24 17:16:54 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

--- Demo Test/components/formView/index.js
+++ Demo Test/components/formView/index.js
@@ -10,7 +10,7 @@ app.formView = kendo.observable({
 
 // END_CUSTOM_CODE_formView
 (function (parent) {
-    var provider = app.data.jsonDataProvider,
+    var provider = app.data.jsonDataProvider(),
         formViewModel = kendo.observable({
             fields: {
                 adress: '',
@@ -18,11 +18,12 @@ app.formView = kendo.observable({
             }
         });
     parent.set('onShow', function (e) {
-		var cepURI = provider(e.view.params.cep);
-        $.get(cepURI, function(result){
-             formViewModel.fields.cep = result.cep;
-             formViewModel.fields.adress = result.logradouro;
-             kendo.bind($("#viewCEP"),formViewModel);
+        provider.read(e.view.params).then(function () {
+            // var view = this.view();
+            console.log('foo:');
+            // formViewModel.fields.cep = result.cep;
+            // formViewModel.fields.adress = result.logradouro;
+            // kendo.bind($("#viewCEP"), formViewModel);
         });
     })
     parent.set('formViewModel', formViewModel);
@@ -31,4 +32,4 @@ app.formView = kendo.observable({
 // START_CUSTOM_CODE_formViewModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-// END_CUSTOM_CODE_formViewModel
+// END_CUSTOM_CODE_formViewModel;
